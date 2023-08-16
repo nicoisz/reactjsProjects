517c02394dcff6b15985fa1b3dd87c899fd8b5a4
nicoisz <nicolasignacio.sz@gmail.com>
2023-08-15 22:37:08 -0400

    estilos en react

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,16 @@
+export function App() {
+  return (
+    <article style={{}}>
+      <header>
+        <img src="https://unavatar.io/twitter/nicotoo_" alt="" />
+        <div>
+          <strong> nicolas silva</strong>
+          <span>@nicotoo</span>
+        </div>
+      </header>
+      <aside>
+        <button>seguir</button>
+      </aside>
+    </article>
+  );
+}
