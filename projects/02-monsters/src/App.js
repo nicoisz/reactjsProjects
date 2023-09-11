ba9068f272deb79a5504ad6c0f37c1e173d3798e
nicoisz <nicolasignacio.sz@gmail.com>
2023-09-11 15:58:20 -0300

    creating filter

--- projects/02-monsters/src/App.js
+++ projects/02-monsters/src/App.js
@@ -8,29 +8,52 @@ class App extends Component {
     this.state = {
       monster: [],
     };
+    console.log("constructor");
   }
 
   componentDidMount() {
+    console.log("componentDidMount");
+
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((value) => value.json())
       .then((users) =>
-        this.setState(
-          () => {
-            return { monster: users };
-          }
-          // () => {
-          //   console.log(this.state);
-          // }
-        )
-      );
+        this.setState(() => {
+          return { monster: users };
+        })
+      )
+      .catch(() => {
+        console.log("error");
+      });
   }
 
   render() {
+    console.log("render");
+
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            let filterText = event.target.value.toLowerCase();
+            let result = this.state.monster.filter((val) =>
+              val.name.toLowerCase().includes(filterText)
+            );
+            console.log(result);
+            this.setState(() => {
+              return { monster: result };
+            });
+          }}
+        ></input>
         <h2>
           {this.state.monster.map((monster) => {
-            return <div key={monster.id}>{monster.name}</div>;
+            return (
+              <div key={monster.id}>
+                {monster.name}
+                <br />
+              </div>
+            );
           })}
         </h2>
       </div>
